21ace913961fb7c70785ddb69ff594dd50b549ad
Alejandro Medina <alex.m@alexmedina.com>
2024-01-27 21:26:49 -0600

    #3 New Implementation of Borda
    A new approach for borda has been implemented, it seems to work, as it gives the same results as the example on the board (which is a good sign given it is the same exact scenario). the documentation might need to be updated in order to give a better explanation of the borda voting rule.

--- voting.py
+++ voting.py
@@ -1,8 +1,25 @@
 import utils
 
+def borda(votes):
+    'In the borda voting strategy, a vote is achieved by adding different points based on the candidate preferences.'
+    'This function goes through a list of votes (given in ranked preferences), and assigns points to the candidate based on their rank.'
+    'This is done for each vote given as a parameter. The number of points added is the priority of the candidate minus the total number of candidates minus one.'
+
+    candidates = {} # this dictionary will be used to keep track of the voting results.
+    for vote in votes: # for each vote, the candidates priority will be used to keep track of scores
+        for i in range(len(vote)): # for every vote entry, all candidates need to be iterated through
+            candidate = vote[i] # candidate will contain the current candidate
+            if candidate in candidates: # if the current candidate already exists, simply add a tally (or point) for them based on borda voting rules
+                candidates[candidate] += len(vote) - i - 1 # update the candidates score by adding the candidates preference minus the total number of candidates minus one 
+            else: # at this point the first candidate does not exist, a new element in the collection of candidates needs to be made
+                candidates[candidate] = len(vote) - i - 1 # update the candidates score by adding the candidates preference minus the total number of candidates minus one 
+      
+    return sorted(candidates.items(), key=lambda x:x[1], reverse=True) # return the sorted results of candidates and their scores
+
+
 def plurality(votes):
     'In plurality voting, a vote is achieved by being the first preference. There are no points associated for second or third ranked choice.'
-    'This method goes through a list of preferences and checks if the candidate ranked first already exists in our collection of candidates.'
+    'This function goes through a list of preferences and checks if the candidate ranked first already exists in our collection of candidates.'
     'If the candidate already exists, then this is the same as someone else voting for this candidate, so the tally increases by one.'
     'If the candidate does not already exist, then this means this is the candidates first vote. By the end, the dictionary will contain the results from the votes.'
     
@@ -21,7 +38,12 @@ def main():
     print("============================================================")
     plurality_result = plurality(votes) # store the results of the plurality voting strategy
     print("Plurality Voting Strategy: " + str(plurality_result)) # print the results using the plurality voting strategy
-    print("Winner: " + str(max(plurality_result))) # announce the winner since the results are not sorted
+    print("Winner: " + str(plurality_result[0])) # announce the winner since the results are not sorted
+    
+    borda_result = borda(votes) # store the results of the borda voting strategy
+    print("\nBorda Voting Strategy: " + str(borda_result)) # print the results using the borda voting strategy
+    print("Winner: " + str(borda_result[0])) # announce the winner since the results are not sorted
+    
     print("============================================================")
 
 
